c627aa249ba691533794720e0d056092216377ac
오한별 <suriessay@naver.com>
2021-06-22 20:45:51 +0900

    인터페이스 연습

---
+++ class-note/3_interface.ts
@@ -0,0 +1,59 @@
+interface User {
+    age:number;
+    name: string;
+}
+
+var mama: User = {
+    age: 33,
+    name : 'nana'
+}
+
+function getUser(user: User) {
+    console.log(user);
+}
+const nana = {
+    name: 'nana',
+    age: 24
+}
+getUser(nana);
+
+interface SumFunction {
+    (a:number,b:number):number;
+}
+
+var sum: SumFunction;
+sum = function(a:number,b:number):number {
+    return a+b;
+}
+
+interface StringArray {
+    [index:number]: string;
+}
+
+var arr:StringArray = ['a','b','c'];
+
+interface StringRegexDictionary {
+    [key: string]:RegExp;
+}
+
+var obj: StringRegexDictionary = {
+    cssFile: /\.css$/,
+    jsFile: /\.js$/,
+}
+
+Object.keys(obj).forEach(function(value){});
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person{
+    language: string;
+}
+
+var aa: Developer = {
+    name:'nana',
+    age: 27,
+    language: 'rrr'
+}
